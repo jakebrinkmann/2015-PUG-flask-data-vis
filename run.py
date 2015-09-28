6fc662aadd2cc1087a4beb7a3efcf16e596f60a7
Jake Brinkmann <jake.brinkmann@ssaihq.com>
2015-09-27 23:50:23 -0400

    Route with fork ribbon

--- run.py
+++ run.py
@@ -1,12 +1,16 @@
 ''' A very simple Flask Hello World app for you to get started with...'''
 
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
+@app.route('/ribbon')
+def show_ribbon():
+    return '<a href="https://github.com/jakebrinkmann/2015-PUG-flask-data-vis/"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/652c5b9acfaddf3a9c326fa6bde407b87f7be0f4/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png"></a>'
+
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask!'
+    return '<a href="'+ url_for('show_ribbon') +'">Click Here</a>'
 
 if __name__ == '__main__':
     app.run()

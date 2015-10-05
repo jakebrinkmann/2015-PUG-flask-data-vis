5f14689d5c35ca3455b0d5f74dc789a1672f32b4
jake-brinkmann <jakebrinkmann@gmail.com>
2015-10-05 09:12:46 -0400

    Static Matplotlib plotting

--- app/views.py
+++ app/views.py
@@ -1,19 +1,38 @@
-from flask import render_template, request
+from flask import render_template, request, make_response
 from flask_wtf import Form
 from wtforms.fields.html5 import DecimalRangeField
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import random
+import StringIO
 from app import app
 
 class MyForm(Form):
     my_slider = DecimalRangeField('Slider')
 
+@app.route('/<int:mag>/plot.png')
+def plot(mag):
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+
+    xs = range(100)
+    ys = [mag * random.randint(1, 50) for x in xs]
+
+    axis.plot(xs, ys)
+    canvas = FigureCanvas(fig)
+    output = StringIO.StringIO()
+    canvas.print_png(output)
+    response = make_response(output.getvalue())
+    response.mimetype = 'image/png'
+    return response
+
 @app.route('/', methods=('GET', 'POST'))
 def index():
     form = MyForm()
-    user = {'nickname': 'Dr. Robert'}
+    magnitude = 50
     if request.method == 'POST':
-        value = request.form['my_slider']
-        user['nickname'] = value
+        magnitude = float(request.form['my_slider'])
     return render_template('index.html',
                            title='Home',
-                           user=user,
+                           mag=magnitude,
                            form=form)

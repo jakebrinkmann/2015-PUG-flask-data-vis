10ab8a7c8150c5300a2d529b87ce45782f57cb1e
jake-brinkmann <jakebrinkmann@gmail.com>
2015-10-05 07:54:20 -0400

    Hello World, app layout

--- run.py
+++ run.py
@@ -1,15 +1,2 @@
-from flask import Flask, url_for
-
-app = Flask(__name__)
-
-@app.route('/proj')
-def projects():
-    return '<h1>Project Page</h1><p><ul><li>PUG Data Visualization</li></ul></p>'
-
-
-@app.route('/')
-def hello_world():
-    return '<a href="'+ url_for('projects') +'">Projects Page</a>'
-
-if __name__ == '__main__':
-    app.run()
+from app import app
+app.run(debug=True)

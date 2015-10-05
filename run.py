e7b527bd99507e940f8b2ca2ff83ddbc3e21749c
jake-brinkmann <jakebrinkmann@gmail.com>
2015-10-05 07:40:37 -0400

    Rendering HTML

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__) # WSGI application
+app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+def show_html():
+    return '<h1>Big heading</h1><p>And some <b>paragraph</b> text</p>'
 
 if __name__ == '__main__':
     app.run()

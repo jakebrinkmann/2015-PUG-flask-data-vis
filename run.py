1c25494a3f7d41286b5da08f4bb02a102c006a02
jake-brinkmann <jakebrinkmann@gmail.com>
2015-10-05 07:47:04 -0400

    URL Routing

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
+@app.route('/proj')
+def projects():
+    return '<h1>Project Page</h1><p><ul><li>PUG Data Visualization</li></ul></p>'
+
+
 @app.route('/')
-def create_error():
-    return 'a %s' % a
+def hello_world():
+    return '<a href="'+ url_for('projects') +'">Projects Page</a>'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()

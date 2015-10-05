21420ac17f058037520ef756d94fc5fd1bd38478
jake-brinkmann <jakebrinkmann@gmail.com>
2015-10-05 08:12:23 -0400

    URL Variables

--- app/views.py
+++ app/views.py
@@ -1,10 +1,15 @@
 from flask import render_template
 from app import app
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'nickname': 'Dr. Robert'}
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/<username>')
+@app.route('/index/<username>')
+def index(username):
+    user = {'nickname': username}
     return render_template('index.html',
                            title='Home',
                            user=user)

7fec73c17d0446f0cff48cae965c11dad91e98bd
jake-brinkmann <jakebrinkmann@gmail.com>
2015-10-05 23:55:05 -0400

    Interactive plot with mpld3

--- app/views.py
+++ app/views.py
@@ -1,30 +1,21 @@
 from flask import render_template, request, make_response
 from flask_wtf import Form
 from wtforms.fields.html5 import DecimalRangeField
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+import mpld3
+from mpld3 import plugins
 import random
-import StringIO
 from app import app
 
 class MyForm(Form):
     my_slider = DecimalRangeField('Slider')
 
-@app.route('/<int:mag>/plot.png')
-def plot(mag):
-    fig = Figure()
-    axis = fig.add_subplot(1, 1, 1)
-
+def draw_plot(mag):
+    fig, ax = plt.subplots()
     xs = range(100)
     ys = [mag * random.randint(1, 50) for x in xs]
-
-    axis.plot(xs, ys)
-    canvas = FigureCanvas(fig)
-    output = StringIO.StringIO()
-    canvas.print_png(output)
-    response = make_response(output.getvalue())
-    response.mimetype = 'image/png'
-    return response
+    ax.scatter(xs, ys)
+    return mpld3.fig_to_html(fig)
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
@@ -32,7 +23,8 @@ def index():
     magnitude = 50
     if request.method == 'POST':
         magnitude = float(request.form['my_slider'])
+    fig = draw_plot(magnitude)
     return render_template('index.html',
                            title='Home',
-                           mag=magnitude,
-                           form=form)
+                           form=form,
+                           figure=fig)
